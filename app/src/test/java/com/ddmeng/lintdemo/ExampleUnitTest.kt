ebd74d78d450ffc02b8823e16824c44eac939485
Dandan Meng <mengdandanno1@163.com>
2020-02-06 11:17:20 +0800

    run ktlintFormat

--- app/src/test/java/com/ddmeng/lintdemo/ExampleUnitTest.kt
+++ app/src/test/java/com/ddmeng/lintdemo/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.ddmeng.lintdemo
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
